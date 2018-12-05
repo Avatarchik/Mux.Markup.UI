1e0ab4b4543ae1b05e55ec01ace2cf94c58981c3
Akihiko Odaki <nekomanma@pixiv.co.jp>
2018-12-05 09:10:21 +0900

    Fix a formatting error in Mux.Markup.UI/AssemblyInfo.cs

--- Mux.Markup.UI/AssemblyInfo.cs
+++ Mux.Markup.UI/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("pixiv Inc.")]
-[assembly: AssemblyCopyright("© pixiv Inc. 2018")]
+[assembly: AssemblyCopyright("\u00A9 pixiv Inc. 2018")]
 [assembly: AssemblyDescription("Unity UI package wrapper for Mux XAML")]
 [assembly: AssemblyTitle("Mux.Markup.UI")]
 [assembly: AssemblyVersion("0.0.*")]
